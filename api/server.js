2cd08a97c3a5fd73d250a4d253f294befc0eb704
Sean Culliton <smculliton@gmail.com>
2023-05-19 10:59:16 -0600

    vercel

--- api/server.js
+++ api/server.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
+const app = require('express')();
+const { v4 } = require('uuid');
 
-dotenv.config()
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
-const app = express()
-const port = 3002
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
-app.use(express.json(), cors())
-
-const sheetsRouter = require('../app/routes/sheets_router')
-app.use('/api/v1/sheets', sheetsRouter)
-
-app.listen(port, () => console.log(`app listening on port ${port}`))
+module.exports = app;
